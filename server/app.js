d97abc0c547e0e6b85b8297fe23a9366fe682a08
Raman Samavedam <a.raman.samavedam@accenture.com>
2017-03-22 12:42:34 +0530

    Added signUp functionality and routing

--- server/app.js
+++ server/app.js
@@ -1,25 +1,22 @@
 /*jshint node:true*/
-
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var compress = require('compression');
 var cors = require('cors');
 var port = process.env.PORT || 4000;
-var routes;
+var index = require('./routes/index');
 var jsonParser = bodyParser.json();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(jsonParser);
 app.use(compress());
 app.use(cors());
-
-routes = require('./routes/index.js');
+app.use('/', index);
 
 console.log('About to start the node');
-
-app.post('/login', routes.signIn);
-app.post('/logout', routes.signOut);
-app.post('/createAdmin', jsonParser, routes.createAdmin);
-
 app.listen(port, function() {
     console.log('Environment = ' + app.get('env') +
                 '\n__dirname = ' + __dirname +
